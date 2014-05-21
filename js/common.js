f199722367f44e5a4a6990d50df1d5c6341bc015
Eugene Godun <eugene.godun@gmail.com>
2014-05-21 22:15:12 +0300

    fixed "document click" bug on ios

--- js/common.js
+++ js/common.js
@@ -1,3 +1,11 @@
 head.ready(function() {
+
+	var agent = navigator.userAgent,
+	event = (agent.match(/iPad/i)) ? "touchstart" : "click";
+
+	$(document).bind(event, function(e){
+		$(".js-popup").hide();
+	});
+
 	console.log($('body').html());
 });
